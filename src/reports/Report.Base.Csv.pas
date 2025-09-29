ceaa08a3d19dd56fc2beb19656f591785b3cf87d
Bernd Berends <bernd.berends@gmail.com>
2025-09-29 22:26:21 +0200

    fFieldsToExport

--- src/reports/Report.Base.Csv.pas
+++ src/reports/Report.Base.Csv.pas
@@ -2,24 +2,34 @@ unit Report.Base.Csv;
 
 interface
 
-uses System.Generics.Collections, InterfacedBase, SqlConnection, Exporter.Types, CsvWriter;
+uses System.Generics.Collections, Data.DB, InterfacedBase, SqlConnection, Exporter.Types, CsvWriter;
 
 type
+  TReportCsvValueToStrEvent = reference to procedure(const aField: TField; var aValue: string);
+
   TReportCsvField = class
+  strict private
+    fFieldName: string;
+    fValueToStrEvent: TReportCsvValueToStrEvent;
   public
-    FieldName: string;
+    constructor Create(const aFieldName: string); overload;
+    constructor Create(const aFieldName: string; const aValueToStrEvent: TReportCsvValueToStrEvent); overload;
+    property FieldName: string read fFieldName;
+    property ValueToStrEvent: TReportCsvValueToStrEvent read fValueToStrEvent;
   end;
 
   TReportBaseCsv = class(TInterfacedBase, IExporterTargetConfig, IExporterRequiresFilePath)
   strict private
-    fFilePath: string;
     fFieldsToExport: TObjectList<TReportCsvField>;
+    fFilePath: string;
     function GetTitle: string;
     function GetResultMessageRequired: Boolean;
     function GetFilePath: string;
     procedure SetFilePath(const aValue: string);
+    function GetFieldsToExportInternal(const aFields: TFields): TList<TPair<TField, TReportCsvValueToStrEvent>>;
   strict protected
     function GetSuggestedFileName: string; virtual;
+    procedure FillFieldsToExport(const aFields: TObjectList<TReportCsvField>); virtual;
     function ExportDataSet(const aDataSet: ISqlDataSet): TExporterExportResult;
     procedure Assign(const aExporterRequiresFilePath: IExporterRequiresFilePath);
   public
@@ -53,30 +63,59 @@ end;
 function TReportBaseCsv.ExportDataSet(const aDataSet: ISqlDataSet): TExporterExportResult;
 begin
   Result := default(TExporterExportResult);
-  var lFileStream := TFileStream.Create(fFilePath, fmCreate or fmOpenWrite);
+  var lFieldsToExport := GetFieldsToExportInternal(aDataSet.DataSet.Fields);
   try
-    var lCsvWriter := TCsvWriter.GetInstance(lFileStream);
-    for var i in aDataSet.DataSet.FieldDefs do
-    begin
-      lCsvWriter.AddValue(i.DisplayName);
-    end;
-    lCsvWriter.NewLine;
-    while not aDataSet.DataSet.Eof do
-    begin
-      for var i in aDataSet.DataSet.Fields do
-      begin
-        var lValue := i.Value;
-        var lValueStr := VarToStr(lValue);
-        lCsvWriter.AddValue(lValueStr);
-      end;
+    var lFileStream := TFileStream.Create(fFilePath, fmCreate or fmOpenWrite);
+    try
+      var lCsvWriter := TCsvWriter.GetInstance(lFileStream);
+      for var i in lFieldsToExport do
+        lCsvWriter.AddValue(i.Key.FieldName);
       lCsvWriter.NewLine;
-      aDataSet.DataSet.Next;
+      while not aDataSet.DataSet.Eof do
+      begin
+        for var i in lFieldsToExport do
+        begin
+          var lValue := i.Key.Value;
+          var lValueStr := VarToStr(lValue);
+          if Assigned(i.Value) then
+            i.Value(i.Key, lValueStr);
+          lCsvWriter.AddValue(lValueStr);
+        end;
+        lCsvWriter.NewLine;
+        aDataSet.DataSet.Next;
+      end;
+      Result.Sucessful := True;
+      Result.FilePath := fFilePath;
+    finally
+      lFileStream.Free;
     end;
-    Result.Sucessful := True;
-    Result.FilePath := fFilePath;
   finally
-    lFileStream.Free;
+    lFieldsToExport.Free;
   end;
+end;
+
+function TReportBaseCsv.GetFieldsToExportInternal(const aFields: TFields): TList<TPair<TField, TReportCsvValueToStrEvent>>;
+begin
+  Result := TList<TPair<TField, TReportCsvValueToStrEvent>>.Create;
+  fFieldsToExport.Clear;
+  FillFieldsToExport(fFieldsToExport);
+  if fFieldsToExport.Count = 0 then
+  begin
+    for var i in aFields do
+      Result.Add(TPair<TField, TReportCsvValueToStrEvent>.Create(i, nil));
+    Exit;
+  end;
+
+  for var i in fFieldsToExport do
+  begin
+    var lField := aFields.FieldByName(i.FieldName);
+    if Assigned(lField) then
+      Result.Add(TPair<TField, TReportCsvValueToStrEvent>.Create(lField, i.ValueToStrEvent));
+  end;
+end;
+
+procedure TReportBaseCsv.FillFieldsToExport(const aFields: TObjectList<TReportCsvField>);
+begin
 end;
 
 function TReportBaseCsv.GetFilePath: string;
@@ -102,6 +141,20 @@ end;
 function TReportBaseCsv.GetTitle: string;
 begin
   Result := 'CSV-Datei';
+end;
+
+{ TReportCsvField }
+
+constructor TReportCsvField.Create(const aFieldName: string);
+begin
+  Create(aFieldName, nil);
+end;
+
+constructor TReportCsvField.Create(const aFieldName: string; const aValueToStrEvent: TReportCsvValueToStrEvent);
+begin
+  inherited Create;
+  fFieldName := aFieldName;
+  fValueToStrEvent := aValueToStrEvent;
 end;
 
 end.
